2e4f0cd12a0484f54210194ac252f560bf60435f
MacStudent <macstudent@126-imac22.local>
2019-02-13 18:55:12 -0500

    class

--- swiftday4/main.swift
+++ swiftday4/main.swift
@@ -8,5 +8,71 @@
 
 import Foundation
 
+/*class Person
+{
+    var pid:Int=0
+    var fname:String!//! optional wrapping always contains value
+    var Lname:String?//?may or maynot value
+    func setData()
+    {
+        fname="tom"
+        Lname="jerry"
+    }
+    
+    func display()
+{
+    print(pid)
+    print (fname)
+   // print (Lname)
+   // if let f = fname, let l = Lname{
+      //  let s=f + " " + 1
+        
+     //   print s;
+    }
+    
+    
+    }
+}
+var p1=Person()
+
+p1.display()
+    
+p1.setData()*/
+
+class Student
+{
+    var sid: Int
+    var fname: String
+    var lname: String
+    
+    init()
+    {
+        sid = 0
+        fname = String()
+        lname = String()
+        
+    
+}
+    init(sid: Int, fname: String, lname: String)
+{
+    self.sid = sid
+    self.fname = fname
+    self.lname = lname
+    
+    }
+    func display()
+    {
+        print("student id : \(self.sid)")
+        print("student fname : \(self.fname)")
+        
+        print("student lname : \(self.lname)")
+    }
+    
+}
+var s1 = Student()
+s1.display()
+let s2 = Student(sid: 300, fname: "raj", lname: "sam")
+s2.display()
+
 
 
